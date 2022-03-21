10936a8e0a763545f1e16fd8d2efe11b5b4c5886
Yang Cheng <annacheng09@hotmail.com>
2022-03-21 16:17:42 -0700

    added questions

--- index.js
+++ index.js
@@ -4,7 +4,49 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // An array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of the project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Please give a description for the project.',
+        name: 'description'     
+    },
+    {
+        type: 'input',
+        message: 'Please give instructions for the installation.',
+        name: 'installation'
+    },
+    {
+        type: 'list',
+        message: 'Please choose the license for this project',
+        name: 'license',
+        choices: ['Apache 2.0', 'GNU GPL v3', 'MIT', 'MPL 2.0', 'none']
+    },
+    {
+        type: 'input',
+        message: 'Please give information on the usage',
+        name: 'usage'   
+    },
+    {
+        type: 'input',
+        message: 'Please give information on contributing guidelines',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'Please provide instructions on how to run tests',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'Please type your Github username',
+        name: 'githubUsername'
+    }
+];
 
 // Function to write README file
 function writeToFile(fileName, data) {}
