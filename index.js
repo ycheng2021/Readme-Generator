615029928155cb2bb4882ef23901ed10be1478aa
Yang Cheng <annacheng09@hotmail.com>
2022-03-21 16:12:28 -0700

    add in index and utils

---
+++ index.js
@@ -0,0 +1,16 @@
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
+
+// An array of questions for user input
+const questions = [];
+
+// Function to write README file
+function writeToFile(fileName, data) {}
+
+// Function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,16 @@
+// Function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// Function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// Function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// Function to generate markdown for README
+function generateMarkdown(data) {}
+
+module.exports = generateMarkdown;
